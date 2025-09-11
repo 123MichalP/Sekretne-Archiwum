0d3ec8b11b54dab432d090d195bf72059d037043
Michal <michal.popieko@tm1.edu.pl>
2025-09-11 11:03:36 +0200

    React confiugration. Added basic html+css main page.

---
+++ sekretne-archiwum/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Sekretne archiwum</h1>
+        <p>Rozwiąż zagadki i odkryj tajemnicę...</p>
+      </header>
+      <main></main>
+      <footer>
+        <p>Więcej zagadek wkrótce! Kontakt: <a href="mailto:michal.popieko@tm1.edu.pl">mail</a><br />
+        Copyright © 2025 SekretneArchwium</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
