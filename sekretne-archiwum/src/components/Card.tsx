11907aee26b33d108d6c08d808e57be7627903eb
MichaelP <michal.popieko@tm1.edu.pl>
2025-10-09 12:08:10 +0200

    Added popup content (+markdown for string). Created system for passwords and unlocking new cards.

--- sekretne-archiwum/src/components/Card.tsx
+++ sekretne-archiwum/src/components/Card.tsx
@@ -1,12 +1,18 @@
 import "../App.css";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Popup from "./Popup";
+import ReactMarkdown from "react-markdown";
+
 interface Props {
   heading: string;
   description: string;
   icon: string;
   initialCardStatus: string;
+  popupContent: string;
+  hasPopupInput: boolean;
   onStatusChange?: (newStatus: string) => void;
+  password: string | number | null;
+  onUnlockNext?: () => void;
 }
 
 function Card({
@@ -15,9 +21,19 @@ function Card({
   icon,
   initialCardStatus,
   onStatusChange,
+  popupContent,
+  hasPopupInput,
+  password,
+  onUnlockNext,
 }: Props) {
   const [buttonPopup, setButtonPopup] = useState(false);
   const [cardStatus, setCardStatus] = useState(initialCardStatus);
+  const [inputValue, setInputValue] = useState("");
+  const [reactionText, setReactionText] = useState("");
+
+  useEffect(() => {
+    setCardStatus(initialCardStatus);
+  }, [initialCardStatus]);
 
   const handleClick = () => {
     if (cardStatus !== "locked") {
@@ -27,7 +43,21 @@ function Card({
     }
   };
 
+  const handleConfirm = () => {
+    console.log(password)
+    console.log(inputValue)
+
+    if(inputValue == password){
+      setReactionText("DOBRZEEEEEEEEEEE!")
+      onUnlockNext?.();
+    }
+    else{
+      setReactionText("ŹLEEEEEEEEEE!")
+    }
+  }
+
   return (
+    <>
     <div className="card" onClick={handleClick}>
       <div dangerouslySetInnerHTML={{ __html: icon }} />
       <h4>{heading}</h4>
@@ -39,13 +69,34 @@ function Card({
           ? "Nie zobaczono"
           : "Zablokowane"}
       </span>
-
-      {cardStatus !== "locked" ? (
-        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-          <h3>{heading}</h3>
-        </Popup>
-      ) : null}
     </div>
+    {cardStatus !== "locked" && (
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <h2>{heading}</h2>
+          <article className="popupContent">
+            <ReactMarkdown>{popupContent}</ReactMarkdown>
+          </article>
+          {hasPopupInput && (
+            <>
+              <input
+                className="popupInput"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Wpisz hasło"
+              />
+              <button
+                type="button"
+                className="popupButton"
+                onClick={handleConfirm}
+              >
+                Zatwierdź
+              </button>
+              <p className="reactionParagraph">{reactionText}</p>
+            </>
+          )}
+        </Popup>
+      )}
+    </>
   );
 }
 
--- sekretne-archiwum/src/components/Popup.tsx
+++ sekretne-archiwum/src/components/Popup.tsx
@@ -7,13 +7,15 @@ type PopupProps = {
 };
 
 const Popup: React.FC<PopupProps> = ({ trigger, children, setTrigger }) => {
-  if (!trigger) return null;
 
-  return (
-    <article className="popup active" onClick={() => setTrigger(false)}>
+    return (
+      <article
+      className={`popup ${trigger ? "show" : ""}`}
+      onClick={() => setTrigger(false)}
+    >
       <div className="innerPopup" onClick={(e) => e.stopPropagation()}>
         {children}
-        <button onClick={() => setTrigger(false)}>Zamknij</button>
+        {/* <button onClick={() => setTrigger(false)}>Zamknij</button> */}
       </div>
     </article>
   );
